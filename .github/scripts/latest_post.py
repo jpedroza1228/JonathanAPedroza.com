739b434f98942890c1344a644aac9c4919120a6f
Jonathan A. Pedroza <jpedroza1228@berkeley.edu>
2026-05-23 17:24:48 -0700

    included local functions

--- .github/scripts/latest_post.py
+++ .github/scripts/latest_post.py
@@ -11,12 +11,29 @@ def get_posts():
     posts.sort(reverse = True)
     return posts
 
+# for local changes on main website page
+def get_posts_local():
+    posts = os.listdir('./_site/posts')
+    posts.sort(reverse = True)
+    return posts
+
 # get_posts()[0]
 
 # get the title, subtitle, date of the post,
 # and the categories that are covered in the post
 def get_post_metadata(post):
   post = frontmatter.load(f'website/posts/{post}/index.qmd')
+  
+  title = post['title']
+  subtitle = post['subtitle']
+  date = post['date']
+  categories = post['categories']
+
+  return title, subtitle, date, categories
+
+# for local changes on main website page
+def get_post_metadata_local(post):
+  post = frontmatter.load(f'./posts/{post}/index.qmd')
   
   title = post['title']
   subtitle = post['subtitle']
